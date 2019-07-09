29cec5b47c4666950298432614222de2e54fcba4
rodrigo debian <correo.rodrigo@gmail.com>
2019-07-09 14:59:19 -0500

    adding con discon

--- Communicator/EventFactory.cs
+++ Communicator/EventFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 using Communicator.Core;
@@ -8,10 +9,12 @@ namespace Communicator
     public interface IEventFactory<TMetaData>
     {
         Task<Guid> Binary(string eventName, byte[] data, TMetaData meta);
-        Task<Guid> String(string eventName, string data, TMetaData meta);
-        Task<Guid> Json<T>(string eventName, T data, TMetaData meta);
         Task<Guid> Serialized<T>(string eventName, T data, TMetaData meta, Func<T, string> serializer);
-        Task<Guid> StringTo(string eventName, string connectionId, string data, TMetaData meta);
+        Task<Guid> String(string eventName, string data, TMetaData meta);
+
+        Task<Guid> Binary(string eventName, byte[] data, TMetaData meta, IEnumerable<string> to);
+        Task<Guid> Serialized<T>(string eventName, T data, TMetaData meta, Func<T, string> serializer, IEnumerable<string> to);
+        Task<Guid> String(string eventName, string data, TMetaData meta, IEnumerable<string> to);
     }
     internal class EventFactory<TMetaData> : IEventFactory<TMetaData>
     {
@@ -21,36 +24,36 @@ namespace Communicator
         {
             this.Connection = connection;  
             this.DefaultSerializer = serializer;
-        }
-           
-        public Task<Guid> Json<T>(string eventName, T data, TMetaData meta)
+        }                   
+
+        public Task<Guid> Serialized<T>(string eventName, T data, TMetaData meta, Func<T, string> serializer, IEnumerable<string> to)
         {
-            return Serialized(eventName, data, meta, (d) => DefaultSerializer.Serialize(d));
+            return Connection.InvokeAsync<Guid>(EventNames.SendStringTo, to, eventName, DefaultSerializer.Serialize(meta), serializer(data));
+        }
+
+        public Task<Guid> Binary(string eventName, byte[] data, TMetaData meta, IEnumerable<string> to) 
+        {            
+            return Connection.InvokeAsync<Guid>(EventNames.SendBinaryTo, to, eventName, DefaultSerializer.Serialize(meta), data);
+        }            
+
+        public Task<Guid> String(string eventName, string data, TMetaData meta, IEnumerable<string> to) 
+        {            
+            return Connection.InvokeAsync<Guid>(EventNames.SendStringTo, to, eventName, DefaultSerializer.Serialize(meta), data);
         }
 
         public Task<Guid> Serialized<T>(string eventName, T data, TMetaData meta, Func<T, string> serializer)
         {
-            return Connection.InvokeAsync<Guid>(EventNames.SendString, eventName, DefaultSerializer.Serialize(meta), serializer(data));
+            return Serialized(eventName, data, meta, serializer, Array.Empty<string>());
         }
 
         public Task<Guid> Binary(string eventName, byte[] data, TMetaData meta) 
         {            
-            return Connection.InvokeAsync<Guid>(EventNames.SendBinary, eventName, DefaultSerializer.Serialize(meta), data);
-        }
-        
+            return Binary(eventName, data, meta, Array.Empty<string>());
+        }            
+
         public Task<Guid> String(string eventName, string data, TMetaData meta) 
         {            
-            return Connection.InvokeAsync<Guid>(EventNames.SendString, eventName, DefaultSerializer.Serialize(meta), data);            
-        }
-
-        // TODO: 
-        // * Remove EventNames.SendString
-        // * Always use EventNames.SendStringTo sending an array of connection Ids, emtpy means to all.
-        // * Do the same for binary
-        // * Implement Rx version of it using IMessage.cs file
-        public Task<Guid> StringTo(string eventName, string connectionId, string data, TMetaData meta) 
-        {            
-            return Connection.InvokeAsync<Guid>(EventNames.SendStringTo, connectionId, eventName, DefaultSerializer.Serialize(meta), data);
+            return String(eventName, data, meta, Array.Empty<string>());
         }
     }
 }
